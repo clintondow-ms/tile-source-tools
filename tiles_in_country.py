db4a55403a0c12ef38b048a978709d4e60d0d45b
clintondow-ms <79865772+clintondow-ms@users.noreply.github.com>
2021-06-17 10:12:55 -0600

    Added tiles_in_country script

---
+++ tiles_in_country.py
@@ -0,0 +1,93 @@
+import asyncio
+import datetime
+import json
+import math
+import os
+import tempfile
+from multiprocessing import Pool
+
+import aiohttp
+from shapely.geometry import Point, shape
+
+API = 'api-version=2.0'
+BASE = 'https://t-azmaps.azurelbs.com/map/tile'
+COUNTRY = "Kenya"
+CHUNK_SIZE = 2000
+KEY = 'FrvcIwC_84Jv5g8mZ4ezpk8-oVae6vVzufnDlydufyU'
+TILESET = 'microsoft.imagery.v2'
+TMP_DIR = "C:/Users/cdow/Desktop/test/"
+ZOOM = 15
+
+sub = f'subscription-key={KEY}'
+url = f'{BASE}?{sub}&{API}&tilesetId={TILESET}&zoom={ZOOM}'
+
+
+def deg2num(lat_deg, lon_deg, zoom):
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    xtile = int((lon_deg + 180.0) / 360.0 * n)
+    ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+    return (xtile, ytile)
+
+
+def num2deg(xtile, ytile, zoom):
+    n = 2.0 ** zoom
+    lon_deg = xtile / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+    lat_deg = math.degrees(lat_rad)
+    return (lat_deg, lon_deg)
+
+
+async def main(data):    
+    jobs = []
+    session = aiohttp.ClientSession()
+    async with session:
+        for x, y in data:
+            endpoint = url + f'&x={x}&y={y}'
+            job = session.get(endpoint)
+            jobs.append(job)
+        await asyncio.gather(*jobs)
+        
+with open("..\geo-countries\data\countries.geojson") as geojson_file:
+    data = json.load(geojson_file)
+
+features = data['features']
+country = [f for f in features if f['properties']['ADMIN'] == COUNTRY][0]
+buffer = shape(country['geometry']).buffer(0.02)
+envelope = buffer.envelope
+coords = envelope.exterior.coords
+p1, p2 = coords[1], coords[3]
+x1, y1 = deg2num(p2[1], p2[0], ZOOM)
+x2, y2 = deg2num(p1[1], p1[0], ZOOM)
+tiles_in_envelope = []
+for x in range(x1, x2+1):
+    for y in range(y1, y2+1):
+        tiles_in_envelope.append((x, y))
+print("Tiles in envelope: " + str(len(tiles_in_envelope)))
+tiles_in_buffer = []
+for tile in tiles_in_envelope:
+    coord = num2deg(tile[0], tile[1], ZOOM)
+    point = Point(coord[1], coord[0])
+    if point.within(buffer):
+        tiles_in_buffer.append(tile)
+print("Tiles in buffer: " + str(len(tiles_in_buffer)))
+for i in range(0, len(tiles_in_buffer), CHUNK_SIZE):
+    file = tempfile.NamedTemporaryFile(delete=False, dir=TMP_DIR)
+    chunk = tiles_in_buffer[i:i+CHUNK_SIZE]
+    data = json.dumps(chunk)
+    file.write(bytes(data, 'utf-8'))
+print(COUNTRY)
+print(datetime.datetime.now())
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+while len(os.listdir(TMP_DIR)) > 0:
+    for file in os.listdir(TMP_DIR):
+        file = TMP_DIR + file
+        try:
+            with open(file, 'r') as chunk:
+                data = json.load(chunk)
+                asyncio.run(main(data))
+            os.remove(file)
+        except:
+            print("Error " + file)
+            continue
+print(datetime.datetime.now())
